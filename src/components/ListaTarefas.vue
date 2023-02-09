e3680b50e1f9580d5ce59657453e8811478a360c
YuriShalk <yuri.shalk@hotmail.com>
2023-02-09 08:58:16 -0300

    formatação

--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -1,40 +1,26 @@
 <template>
-    <div>
-        <p class="titulo">O que você tem para fazer?</p>
+    <div class="titulo">
+        <p>O que você tem para fazer?</p>
     </div>
     <div class="tarefa">
-        <input 
-        id="input-task"
-        v-model="nomeDaTarefa"
-        type="text"
-        > 
-        <button
-            id="save-button"
-            @click="salvaTarefa()"
-            >
-             Salvar
+        <input id="input-task" v-model="nomeDaTarefa" type="text">
+        <button id="save-button" @click="salvaTarefa()">
+            Salvar
         </button>
     </div>
-    <div 
-        class="lista-itens" 
-        v-for="(tarefa, i) in listaTarefas"
-        :key="tarefa.i"
-    >
-    <button id="delete-button" @click="removeTarefa(i)">&#10060;</button>
-    <button id="complete-button" @click="concluiTarefa(i)">&#9989;</button>
+    <div class="lista-itens" v-for="(tarefa, i) in listaTarefas" :key="tarefa.i">
+        <button id="delete-button" @click="removeTarefa(i)">&#10060;</button>
+        <button id="complete-button" @click="concluiTarefa(i)">&#9989;</button>
 
         {{ tarefa.nomeDaTarefa }}
-    
-    </div>
-<h2>Things done:</h2>
-    <div class="lista-tarefas-completas"
-        v-for="(tarefaCompleta, j) in listaTarefasCompletadas"
-        :key="tarefaCompleta.j"
-    >
 
-    <button id="delete-button" @click="removeTarefaCompletada(j)">&#10060;</button>
-    <button id="return-button" @click="retornaTarefaCompletada(j)">&#8617;</button>
-    {{ tarefaCompleta.nomeDaTarefa }}
+    </div>
+    <h2>Things done:</h2>
+    <div class="lista-tarefas-completas" v-for="(tarefaCompleta, j) in listaTarefasCompletadas" :key="tarefaCompleta.j">
+
+        <button id="delete-button" @click="removeTarefaCompletada(j)">&#10060;</button>
+        <button id="return-button" @click="retornaTarefaCompletada(j)">&#8617;</button>
+        {{ tarefaCompleta.nomeDaTarefa }}
 
     </div>
 </template>
@@ -46,71 +32,71 @@ export default {
         return {
             nomeDaTarefa: '',
             listaTarefas: [
-                {nomeDaTarefa: 'Começar a ler um livro (Exemplo)'},
-                {nomeDaTarefa: 'Meditar 5 minutos (Exemplo)'},
-            ], 
-            
+                { nomeDaTarefa: 'Começar a ler um livro (Exemplo)' },
+                { nomeDaTarefa: 'Meditar 5 minutos (Exemplo)' },
+            ],
+
 
             listaTarefasCompletadas: [
-                {nomeDaTarefa: 'Aqui ficam as tarefas já realizadas!'},
+                { nomeDaTarefa: 'Aqui ficam as tarefas já realizadas!' },
             ],
 
         }
     },
 
     methods: {
-        salvaTarefa(){
-            if(this.nomeDaTarefa.trim() === ''){
+        salvaTarefa() {
+            if (this.nomeDaTarefa.trim() === '') {
                 return;
             }
 
             this.listaTarefas.push({
                 nomeDaTarefa: this.nomeDaTarefa
             })
-            
-            this.nomeDaTarefa= '' 
+
+            this.nomeDaTarefa = ''
 
         },
 
 
-        removeTarefa(i){
+        removeTarefa(i) {
             this.listaTarefas.splice(i, 1);
         },
-        
-        concluiTarefa(i){
-            
+
+        concluiTarefa(i) {
+
             this.listaTarefasCompletadas.push({
-                nomeDaTarefa:' ' + this.listaTarefas[i].nomeDaTarefa
+                nomeDaTarefa: ' ' + this.listaTarefas[i].nomeDaTarefa
             });
-            
+
             this.removeTarefa(i)
         },
 
-        retornaTarefaCompletada(i){
+        retornaTarefaCompletada(i) {
             this.listaTarefas.push({
-                nomeDaTarefa:' ' + this.listaTarefasCompletadas[i].nomeDaTarefa
+                nomeDaTarefa: ' ' + this.listaTarefasCompletadas[i].nomeDaTarefa
             });
-            
+
             this.removeTarefaCompletada(i)
         },
 
-        removeTarefaCompletada(i){
+        removeTarefaCompletada(i) {
             this.listaTarefasCompletadas.splice(i, 1);
 
-        }, 
+        },
 
     },
 
     watch: {
         listaTarefas: {
-            handler(){
+            handler() {
                 localStorage.setItem('listaTarefas', JSON.stringify(this.listaTarefas))
             },
             deep: true
         },
 
         listaTarefasCompletadas: {
-            handler(){
+            handler() {
                 localStorage.setItem('listaTarefasCompletadas', JSON.stringify(this.listaTarefasCompletadas))
             },
             deep: true
@@ -119,84 +105,84 @@ export default {
     },
 
     mounted() {
-            if(localStorage.getItem('listaTarefas', 'listaTarefasCompletadas')){
-                this.listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'))
-                this.listaTarefasCompletadas = JSON.parse(localStorage.getItem('listaTarefasCompletadas'))
-            }
-        },
+        if (localStorage.getItem('listaTarefas', 'listaTarefasCompletadas')) {
+            this.listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'))
+            this.listaTarefasCompletadas = JSON.parse(localStorage.getItem('listaTarefasCompletadas'))
+        }
+    },
 
 }
 </script>
 
 <style>
-    .titulo, .tarefa{
-        font-size: 28px;
-        padding: 3px;
-    }
+.titulo,
+.tarefa {
+    font-size: 28px;
+    padding: 3px;
+}
 
-    .lista-itens{
-        background: rgb(7, 213, 249);
-        color: rgb(8, 8, 8);
-        border-radius: 30px;
-        border: black solid 3px;
-        margin: 8px 0 8px 0;
-        padding: 15px;
-        text-align: left;
-        width: 740px;
-        height: 15px;
-        display: inline-block;
-        
-    }
+.lista-itens {
+    background: rgb(7, 213, 249);
+    color: rgb(8, 8, 8);
+    border-radius: 30px;
+    border: black solid 3px;
+    margin: 8px 0 8px 0;
+    padding: 15px;
+    text-align: left;
+    width: 740px;
+    height: 15px;
+    display: inline-block;
 
-    .lista-tarefas-completas{
-        background: rgb(110, 116, 117);
-        color: rgb(255, 255, 255);
-        border-radius: 30px;
-        border: black solid 3px;
-        margin: 8px 0 8px 0;
-        padding: 15px;
-        text-align: left;
-        text-decoration: line-through;
-        width: 740px;
-        height: 15px;
-        display: inline-block;
-        
-    }
+}
 
-    #save-button{
-        background-color: blue; 
-        color: rgb(247, 94, 39);
-        font-weight: bold;
-        text-align: center;
-        font-size: 28px;
-        margin-top: 15px;
-        margin-left: 20px;
-    }
+.lista-tarefas-completas {
+    background: rgb(110, 116, 117);
+    color: rgb(255, 255, 255);
+    border-radius: 30px;
+    border: black solid 3px;
+    margin: 8px 0 8px 0;
+    padding: 15px;
+    text-align: left;
+    text-decoration: line-through;
+    width: 740px;
+    height: 15px;
+    display: inline-block;
 
-    #save-button:hover{
-        font-size: 32px;
-        background-color: black;
-        color: white;
-        
-    }
+}
 
-    #input-task{
-        font-size: 30px;
-        width: 40%;
-    }
+#save-button {
+    background-color: blue;
+    color: rgb(247, 94, 39);
+    font-weight: bold;
+    text-align: center;
+    font-size: 28px;
+    margin-top: 15px;
+    margin-left: 20px;
+}
 
-    #delete-button{
-        background-color: black;
-        color: white;
-        margin: 0px 0px 0px 690px;
-        display: inline-block;
-        position: absolute;
-    }
+#save-button:hover {
+    font-size: 32px;
+    background-color: black;
+    color: white;
 
-    #delete-button:hover{
-        background-color: black;
-        color: red;
-        font-size: 15px;
-    }
+}
 
+#input-task {
+    font-size: 30px;
+    width: 40%;
+}
+
+#delete-button {
+    background-color: black;
+    color: white;
+    margin: 0px 0px 0px 690px;
+    display: inline-block;
+    position: absolute;
+}
+
+#delete-button:hover {
+    background-color: black;
+    color: red;
+    font-size: 15px;
+}
 </style>
